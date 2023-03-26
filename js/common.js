0864aff3743e69ac2850aa2c78f0d4156f5f3365
Gaga <regalue@gmail.com>
2023-03-26 22:15:55 +0800

    update

--- js/common.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $(window).on("scroll", function() {
-        if ( $(window).scrollTop() > 50) {
-            $(".header-nav .nav").addClass("-open-logo")
-        } else {
-            $(".header-nav .nav").removeClass("-open-logo")
-        }
-    })
-})
