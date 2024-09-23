72ac12061e841069ca19c26ee5f9265e3decaca7
GAGA <gagalu@cw.com.tw>
2024-09-23 15:24:14 +0800

    [update] update company name and files
    [update] update company name and files

--- en/js/common.js
+++ en/js/common.js
@@ -1,4 +1,9 @@
 $(function () {
+    let thisYear = new Date().getFullYear();
+    let year = $('#year');
+      if (year.length) {
+          year.text(thisYear);
+      }
     $(window).on("scroll", function() {
         if ( $(window).scrollTop() > 50) {
             $(".header-nav .nav").addClass("-open-logo")
--- js/common.js
+++ js/common.js
@@ -1,4 +1,11 @@
 $(function () {
+
+  let thisYear = new Date().getFullYear();
+  let year = $('#year');
+    if (year.length) {
+        year.text(thisYear);
+    }
+
     $(window).on("scroll", function() {
         if ( $(window).scrollTop() > 50) {
             $(".header-nav .nav").addClass("-open-logo")
@@ -6,4 +13,6 @@ $(function () {
             $(".header-nav .nav").removeClass("-open-logo")
         }
     })
+
+
 })
